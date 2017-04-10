4cc9ab33af2394f376c471d85f625715aa12cda3
j9recurses <janine.heiser@gmail.com>
2017-04-10 13:12:53 -0700

    fixes typo in config file

--- pydev/system_updt_emails.py
+++ pydev/system_updt_emails.py
@@ -72,14 +72,14 @@ def main():
   base_email_txt = ec.getBaseMsgText('systems_updt')
   subject_line = e._emailConfigs['email_situations']['systems_updt']['subject_line']
   wkbks_dir = configItems['wkbk_dir']+ '/blank_wkbks/'
-  for item in email_list:
+  for item in email_list[0:2]:
     msgBody  = base_email_txt % (item['coordinator_name'], item['template_file_name'])
     attachment_dictList = [{item['template_file_name']: wkbks_dir+item['template_file_name']},{ 'InventoryUpdateGuidance.pdf': wkbks_dir+'InventoryUpdateGuidance.pdf'}]
-    recipientsActual = {'To':item['primary_data_coordinator']}
+    recipientsActual = {'To':item['primary_data_coordinator'], 'bcc': 'jason.lally@sfgov.org'}
     if 'secondary_data_coordinator' in item.keys():
       recipientsActual['cc'] = item['secondary_data_coordinator']
     print recipientsActual
-    mail_recipient = e.sendEmails(subject_line, msgBody, fname_attachment=None, fname_attachment_fullpath=None, recipients={ 'To': 'jason.lally@sfgov.org', 'cc':'janine.heiser@sfgov.org', 'bcc': 'jason.lally@sfgov.org'}, attachment_dictList = attachment_dictList, isETL=False)
+    mail_recipient = e.sendEmails(subject_line, msgBody, fname_attachment=None, fname_attachment_fullpath=None, recipients={ 'To': 'janine.heiser@sfgov.org', 'cc':'janine.heiser@sfgov.org', 'bcc': 'janine.heiser@sfgov.org'}, attachment_dictList = attachment_dictList, isETL=False)
 
 if __name__ == "__main__":
     main()
