4459b5575113c35eae8da0bc0e565c5d8ee4443e
j9recurses <janine.heiser@gmail.com>
2017-06-06 13:20:09 -0700

    fixes casting issues

--- pydev/screendoor_and_upload.py
+++ pydev/screendoor_and_upload.py
@@ -183,6 +183,7 @@ def main():
         if sht == 'Systems Inventory':
           df['required_fields_count'] =  df.apply(lambda row: getRequiredFieldCountInventory(row), axis=1)
           df['required_fields_complete'] =  df.apply(lambda row:getRequiredFieldsCompleteInventory(row), axis=1)
+          print df
           df = df[df['department_custodian'] != '' ].reset_index()
         elif sht == 'Dataset Inventory':
           df['required_fields_count'] =  df.apply(lambda row: getRequiredFieldCountDatasets(row), axis=1)
