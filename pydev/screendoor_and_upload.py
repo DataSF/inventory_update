852608fdcf8b19b29c4a2bb92c90849374547ab5
j9recurses <janine.heiser@gmail.com>
2017-06-06 13:35:08 -0700

    fixes casting issues

--- pydev/screendoor_and_upload.py
+++ pydev/screendoor_and_upload.py
@@ -215,7 +215,7 @@ def main():
     for result in results:
       print results
       result['submitted'] = True
-      result['submitted_systems_row_count'] = int(getSubmittedCnt(sQobj,base_url, fbf_systems_inventory, 'department_custodian', 'submitted_systems_row_count', str(result['department_or_division']).strip())
+      result['submitted_systems_row_count'] = int(getSubmittedCnt(sQobj,base_url, fbf_systems_inventory, 'department_custodian', 'submitted_systems_row_count', str(result['department_or_division']).strip()     ))
       result['systems_required_total'] = int(getSums(sQobj,base_url, fbf_systems_inventory, 'required_fields_count', 'department_custodian', result['department_or_division'] ))
       result['systems_required_complete'] =  int(getSums(sQobj,base_url, fbf_systems_inventory, 'required_fields_complete', 'department_custodian', result['department_or_division'] ))
       result['systems_required_remaining'] = result['systems_required_total'] - result['systems_required_complete']
@@ -223,8 +223,8 @@ def main():
       result['datasets_required_complete'] =  int(getSums(sQobj,base_url, fbf_datasets_inventory , 'required_fields_complete', 'department_or_division', result['department_or_division'] ))
       result['datasets_required_remaining'] = result['datasets_required_total'] - result['datasets_required_complete']
   #dsse = JobStatusEmailerComposer(configItems, logger, jobType)
-  dataset_info = {'Socrata Dataset Name': configItems['dd']['index']['dataset_name'], 'SrcRecordsCnt':len(results), 'DatasetRecordsCnt':0, 'fourXFour': configItems['dd']['index']['fbf'], 'row_id': 'department_or_division'}
-  dataset_info = scrud.postDataToSocrata(dataset_info, results )
+  #dataset_info = {'Socrata Dataset Name': configItems['dd']['index']['dataset_name'], 'SrcRecordsCnt':len(results), 'DatasetRecordsCnt':0, 'fourXFour': configItems['dd']['index']['fbf'], 'row_id': 'department_or_division'}
+  #dataset_info = scrud.postDataToSocrata(dataset_info, results )
   #dataset_info['isLoaded'] = 'success'
   #dataset_results.append(dataset_info)
   #dsse.sendJobStatusEmail(dataset_results)
